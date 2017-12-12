142c8f03d7acc11ebf1f3d4b98891633030d1ac9
David Barth <davidbarth80@gmail.com>
2017-12-12 19:41:05 +0000

    Project setup

---
+++ DBTableClasses/ProductType.cs
@@ -0,0 +1,27 @@
+namespace CoffeeShop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ProductType")]
+    public partial class ProductType
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public ProductType()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        [Column("ProductType")]
+        public int ProductType1 { get; set; }
+
+        public string Description { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
